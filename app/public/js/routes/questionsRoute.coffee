b50b03c62c8001a9e2cb872161e959571e6584a0
Shengying Pan <i@thepans.me>
2014-09-15 17:16:04 -0400

    fixing view actions

--- app/public/js/routes/questionsRoute.coffee
+++ app/public/js/routes/questionsRoute.coffee
@@ -4,6 +4,8 @@ define ['jquery', 'me', '/js/MathJax/MathJax.js?config=TeX-AMS-MML_HTMLorMML'
 ($, me) ->
 	QuestionsRoute = 
 		setup: (App) ->
+			me.attach App, ['Question', 'School']
+
 			#route
 			App.Router.map ->
 				@resource 'questions', ->
@@ -21,47 +23,63 @@ define ['jquery', 'me', '/js/MathJax/MathJax.js?config=TeX-AMS-MML_HTMLorMML'
 						thiz.transitionTo 'login'
 
 
+			App.QuestionsNewRoute = Ember.Route.extend
+				model: ->
+					return @store.createRecord 'question', {}
+
+
 			#m
 
 			#v
-			
-			App.QuestionsNewController = Ember.ObjectController.extend
+			App.QuestionsNewView = Ember.View.extend
 				didInsertElement: ->
 					@_super()
-					Preview = 
-						delay: 150
-						preview: null
-						buffer: null
-						timeout: null
-						mjRunning: false
-						oldText: null
-						init: ->
-							@preview = document.getElementById 'math-preview'
-							@buffer = document.getElementById 'math-buffer'
-						swapBuffers: ->
-							buffer = @preview
-							preview = @buffer
-							buffer.style.visibility = 'hidden'
-							buffer.style.position = 'absolute'
-							preview.style.position = ''
-							preview.style.visibility = ''
-						update: ->
-							if @timeout then clearTimeout @timeout
-							@timeout = setTimeout @callback, @delay
-						createPreview: ->
-							Preview.timeout = null
-							if @mjRunning then return
-							text = document.getElementById('math-input').value
-							if text == @oldtext then return
-							@buffer.innerHTML = @oldtext = text
-							@mjRunning = true
-							MathJax.Hub.Queue ['Typeset', MathJax.Hub, @buffer], ['previewDone', @]
-						previewDone: ->
-							@mjRunning = false
-							@swapBuffers()
 
+					Preview = @controller.get 'Preview'
+					Preview.init()
 					Preview.callback = MathJax.Callback ['createPreview', Preview]
 					Preview.callback.autoReset = true
+
+			#c
+			App.QuestionsNewController = Ember.ObjectController.extend
+				Preview:
+					delay: 150
+					preview: null
+					buffer: null
+					timeout: null
+					mjRunning: false
+					oldText: null
+					init: ->
+						@preview = document.getElementById 'math-preview'
+						@buffer = document.getElementById 'math-buffer'
+					swapBuffers: ->
+						buffer = @preview
+						preview = @buffer
+						buffer.style.visibility = 'hidden'
+						buffer.style.position = 'absolute'
+						preview.style.position = ''
+						preview.style.visibility = ''
+					update: ->
+						if @timeout then clearTimeout @timeout
+						@timeout = setTimeout @callback, @delay
+					createPreview: ->
+						@timeout = null
+						if @mjRunning then return
+						text = document.getElementById('math-input').value
+						if text == @oldtext then return
+						@buffer.innerHTML = @oldtext = text
+						@mjRunning = true
+						MathJax.Hub.Queue ['Typeset', MathJax.Hub, @buffer], ['previewDone', @]
+					previewDone: ->
+						@mjRunning = false
+						@swapBuffers()
+				actions:
+					updateMath: ->
+						@Preview.update()
+					add: ->
+						console.log @get('model')
+
+					
 
     		
 
--- app/public/js/routes/questionsRoute.js
+++ app/public/js/routes/questionsRoute.js
@@ -3,6 +3,7 @@ define(['jquery', 'me', '/js/MathJax/MathJax.js?config=TeX-AMS-MML_HTMLorMML', '
   var QuestionsRoute;
   QuestionsRoute = {
     setup: function(App) {
+      me.attach(App, ['Question', 'School']);
       App.Router.map(function() {
         return this.resource('questions', function() {
           return this.route('new');
@@ -21,57 +22,74 @@ define(['jquery', 'me', '/js/MathJax/MathJax.js?config=TeX-AMS-MML_HTMLorMML', '
           });
         }
       });
-      return App.QuestionsNewController = Ember.ObjectController.extend({
+      App.QuestionsNewRoute = Ember.Route.extend({
+        model: function() {
+          return this.store.createRecord('question', {});
+        }
+      });
+      App.QuestionsNewView = Ember.View.extend({
         didInsertElement: function() {
           var Preview;
           this._super();
-          Preview = {
-            delay: 150,
-            preview: null,
-            buffer: null,
-            timeout: null,
-            mjRunning: false,
-            oldText: null,
-            init: function() {
-              this.preview = document.getElementById('math-preview');
-              return this.buffer = document.getElementById('math-buffer');
-            },
-            swapBuffers: function() {
-              var buffer, preview;
-              buffer = this.preview;
-              preview = this.buffer;
-              buffer.style.visibility = 'hidden';
-              buffer.style.position = 'absolute';
-              preview.style.position = '';
-              return preview.style.visibility = '';
-            },
-            update: function() {
-              if (this.timeout) {
-                clearTimeout(this.timeout);
-              }
-              return this.timeout = setTimeout(this.callback, this.delay);
-            },
-            createPreview: function() {
-              var text;
-              Preview.timeout = null;
-              if (this.mjRunning) {
-                return;
-              }
-              text = document.getElementById('math-input').value;
-              if (text === this.oldtext) {
-                return;
-              }
-              this.buffer.innerHTML = this.oldtext = text;
-              this.mjRunning = true;
-              return MathJax.Hub.Queue(['Typeset', MathJax.Hub, this.buffer], ['previewDone', this]);
-            },
-            previewDone: function() {
-              this.mjRunning = false;
-              return this.swapBuffers();
-            }
-          };
+          Preview = this.controller.get('Preview');
+          Preview.init();
           Preview.callback = MathJax.Callback(['createPreview', Preview]);
           return Preview.callback.autoReset = true;
+        }
+      });
+      return App.QuestionsNewController = Ember.ObjectController.extend({
+        Preview: {
+          delay: 150,
+          preview: null,
+          buffer: null,
+          timeout: null,
+          mjRunning: false,
+          oldText: null,
+          init: function() {
+            this.preview = document.getElementById('math-preview');
+            return this.buffer = document.getElementById('math-buffer');
+          },
+          swapBuffers: function() {
+            var buffer, preview;
+            buffer = this.preview;
+            preview = this.buffer;
+            buffer.style.visibility = 'hidden';
+            buffer.style.position = 'absolute';
+            preview.style.position = '';
+            return preview.style.visibility = '';
+          },
+          update: function() {
+            if (this.timeout) {
+              clearTimeout(this.timeout);
+            }
+            return this.timeout = setTimeout(this.callback, this.delay);
+          },
+          createPreview: function() {
+            var text;
+            this.timeout = null;
+            if (this.mjRunning) {
+              return;
+            }
+            text = document.getElementById('math-input').value;
+            if (text === this.oldtext) {
+              return;
+            }
+            this.buffer.innerHTML = this.oldtext = text;
+            this.mjRunning = true;
+            return MathJax.Hub.Queue(['Typeset', MathJax.Hub, this.buffer], ['previewDone', this]);
+          },
+          previewDone: function() {
+            this.mjRunning = false;
+            return this.swapBuffers();
+          }
+        },
+        actions: {
+          updateMath: function() {
+            return this.Preview.update();
+          },
+          add: function() {
+            return console.log(this.get('model'));
+          }
         }
       });
     }
