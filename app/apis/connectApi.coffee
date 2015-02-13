7232aa4c8f08ab7ea848b62b592989dc1170a30b
Shengying Pan <i@thepans.me>
2015-02-13 08:25:38 -0500

    update

--- app/apis/connectApi.coffee
+++ app/apis/connectApi.coffee
@@ -16,31 +16,41 @@ exports.bind = (app) ->
 				#login
 				req.session[config.sessionKey] = user._id.toString()
 
-				Question = me.getModel 'Question'
-				question = new Question question
 
-				#update question
-				username = user.username
-				time = moment().toDate()
-
-				question.editor = user._id
-
-				question.lastEditor = username
-				question.lastModifiedTime = time
-				question.questionLastEditor = username
-				question.questionLastModifiedTime = time
-				question.solutionLastEditor = username
-				question.solutionLastModifiedTime = time
-				question.hintLastEditor = username
-				question.hintLastModifiedTime = time
-				question.summaryLastEditor = username
-				question.summaryLastModifiedTime = time
-
-				question.save (err, question) ->
+				if !question.school? then return res.send 404, 'Question must have an associated school'
+				School = me.getModel 'School'
+				pattern = new RegExp '^' + question.school + '$', 'ig'
+				School.findOne {name: pattern}, (err, school) ->
 					if err
 						res.send 500, err.message
+					else if !school?
+						res.send 404, 'Invalid school for given question'
 					else
-						res.send 200, path.join(config.url, '#/question/' + question._id + '/edit').toString()
+						Question = me.getModel 'Question'
+						question = new Question question
+
+						#update question
+						username = user.username
+						time = moment().toDate()
+
+						question.editor = user._id
+
+						question.lastEditor = username
+						question.lastModifiedTime = time
+						question.questionLastEditor = username
+						question.questionLastModifiedTime = time
+						question.solutionLastEditor = username
+						question.solutionLastModifiedTime = time
+						question.hintLastEditor = username
+						question.hintLastModifiedTime = time
+						question.summaryLastEditor = username
+						question.summaryLastModifiedTime = time
+
+						question.save (err, question) ->
+							if err
+								res.send 500, err.message
+							else
+								res.send 200, path.join(config.url, '#/question/' + question._id + '/edit').toString()
 
 
 	app.post '/api/connect/auth', (req, res) ->
--- app/apis/connectApi.js
+++ app/apis/connectApi.js
@@ -16,31 +16,47 @@ exports.bind = function(app) {
     question = req.body.question;
     token = req.body.token;
     return jwt.verify(token, config.secret, function(err, user) {
-      var Question, time, username;
+      var School, pattern;
       if (err) {
         return res.send(401, 'You do not have the permission to access this api');
       } else {
         req.session[config.sessionKey] = user._id.toString();
-        Question = me.getModel('Question');
-        question = new Question(question);
-        username = user.username;
-        time = moment().toDate();
-        question.editor = user._id;
-        question.lastEditor = username;
-        question.lastModifiedTime = time;
-        question.questionLastEditor = username;
-        question.questionLastModifiedTime = time;
-        question.solutionLastEditor = username;
-        question.solutionLastModifiedTime = time;
-        question.hintLastEditor = username;
-        question.hintLastModifiedTime = time;
-        question.summaryLastEditor = username;
-        question.summaryLastModifiedTime = time;
-        return question.save(function(err, question) {
+        if (question.school == null) {
+          return res.send(404, 'Question must have an associated school');
+        }
+        School = me.getModel('School');
+        pattern = new RegExp('^' + question.school + '$', 'ig');
+        return School.findOne({
+          name: pattern
+        }, function(err, school) {
+          var Question, time, username;
           if (err) {
             return res.send(500, err.message);
+          } else if (school == null) {
+            return res.send(404, 'Invalid school for given question');
           } else {
-            return res.send(200, path.join(config.url, '#/question/' + question._id + '/edit').toString());
+            Question = me.getModel('Question');
+            question = new Question(question);
+            username = user.username;
+            time = moment().toDate();
+            question.editor = user._id;
+            question.lastEditor = username;
+            question.lastModifiedTime = time;
+            question.questionLastEditor = username;
+            question.questionLastModifiedTime = time;
+            question.solutionLastEditor = username;
+            question.solutionLastModifiedTime = time;
+            question.hintLastEditor = username;
+            question.hintLastModifiedTime = time;
+            question.summaryLastEditor = username;
+            question.summaryLastModifiedTime = time;
+            return question.save(function(err, question) {
+              if (err) {
+                return res.send(500, err.message);
+              } else {
+                return res.send(200, path.join(config.url, '#/question/' + question._id + '/edit').toString());
+              }
+            });
           }
         });
       }
